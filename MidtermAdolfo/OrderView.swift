3c725ca2c064408648d861ce3556043ce7f8087d
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-10-16 17:44:26 -0400

    Added buttons and pickers for orderview

--- MidtermAdolfo/OrderView.swift
+++ MidtermAdolfo/OrderView.swift
@@ -12,14 +12,36 @@ struct OrderView: View {
     @State private var selectedSize: PizzaSizes = .medium
     @State private var selectedTopping: PizzaToppings = .pepperoni
     @State private var selectedQuantity: String = "" //Will Int() this
+    @State private var isPeperoni = false
+    @State private var isVeggie = false
+    @State private var isCheese = false
     var body: some View {
         NavigationStack{
-            TextField("Enter user name",
-                      text: $userName
-            )
-            TextField("Enter number of Pizza's",
-                      text: $selectedQuantity
-            )
+            VStack{
+                List{
+                    TextField("Enter user name",
+                              text: $userName
+                    )
+                    Picker("Select Pizza Size:", selection: $selectedSize){
+                        Text("Small").tag(PizzaSizes.small)
+                        Text("Medium").tag(PizzaSizes.medium)
+                        Text("Large").tag(PizzaSizes.large)
+                    }.pickerStyle(.menu)
+                    Text("Choose Toppings:")
+                    HStack{
+                        Picker("Select Pizza Toppings:", selection: $selectedTopping){
+                            Text("Cheese Pizza").tag(PizzaToppings.cheese)
+                            Text("Pepperoni Pizza").tag(PizzaToppings.pepperoni)
+                            Text("Veggie Pizza").tag(PizzaToppings.veggie)
+                        }.pickerStyle(.segmented)
+                    }
+                    TextField("Enter number of Pizza's",
+                              text: $selectedQuantity
+                    )
+                }
+                
+                
+            }
         }
     }
 }
