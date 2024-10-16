bd716986ba5dee99e07191fe5d9a8872a95c2101
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-10-16 17:06:31 -0400

    Initial Commit

---
+++ MidtermAdolfo/MidtermAdolfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermAdolfoApp.swift
+//  MidtermAdolfo
+//
+//  Created by David Romero on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct MidtermAdolfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
